1c294c1975be5557d1e4bb61d9ca5a0d4bfa686d
Luis Vega <alexis_ples@yahoo.com>
2021-02-02 22:13:57 -0500

    testing ssh-key...

--- src/main.cxx
+++ src/main.cxx
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(int argc, char* arg[]) {
-	
+	std::cout << "Testing ssh-keys... \n";
 
 	std::cout << "End of Program. (Press any key to continue... )" << std::endl;
 	std::cin.get();
